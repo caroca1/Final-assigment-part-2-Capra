49b72fcf0662072e80f0b0f8bedab9d72597ece4
caroca1 <147034538+caroca1@users.noreply.github.com>
2023-12-20 09:32:05 +0100

    Update DV0101EN-Final_Assign_Part_2_Questions.py

--- DV0101EN-Final_Assign_Part_2_Questions.py
+++ DV0101EN-Final_Assign_Part_2_Questions.py
@@ -33,32 +33,26 @@ year_list = [i for i in range(1980, 2024, 1)]
 #---------------------------------------------------------------------------------------
 # Create the layout of the app
 app.layout = html.Div([
-    #TASK 2.1 Add title to the dashboard
-    html.H1('Automobile Sales Statistics Dashboard', style={'textAlign': 'center', 'color': '#503D36', 'font-size': 24}), # May include style for title
-
-html.Div([#TASK 2.2: Add two dropdown menus
-    html.Label("Select Statistics:"),
-    dcc.Dropdown(
-        id='dropdown-statistics', 
-        options=[
-            {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
-            {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}
-                ],
-        placeholder='Select a report type.',
-        style={'width': '80%', 'padding': '3px', 'font-size': 20, 'textAlign': 'center'})
+    html.H1("Automobile Statistics Dashboard"),
+    html.Div([
+        html.Label("Select Statistics:"),
+        dcc.Dropdown(
+            id='dropdown-statistics',
+            options=dropdown_options,
+            value='Select Statistics'
+        )
     ]),
-
-html.Div([
-    html.Label("Select Year:"),
-    dcc.Dropdown(
-        id='select-year', 
-        options=[{'label': i, 'value': i} for i in year_list],
-        placeholder='Select a year',
-        style={'width': '80%', 'padding': '3px', 'font-size': 20, 'textAlign': 'center'})]),
+    html.Div(dcc.Dropdown(
+            id='select-year',
+            options=[{'label': i, 'value': i} for i in year_list],
+            value='Select-year'
+        )),
     
-html.Div([#TASK 2.3: Add a division for output display
-html.Div(id='output-container', className='chart-grid', style={'flex'}),])
+    html.Div([
+    html.Div(id='output-container', className='chart-grid', style={'display': 'flex'}),
+    ])
 ])
+
 #TASK 2.4: Creating Callbacks
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
